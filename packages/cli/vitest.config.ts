3f70bd21455355f8e468db41a48e7be16674eef2
pessi-v <pessimo@posteo.net>
2025-08-06 18:09:16 +0200

    work on test config

--- packages/cli/vitest.config.ts
+++ packages/cli/vitest.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
     environment: "node",
     include: ["**/test/**/*.test.ts", "**/test/**/*.test.js"],
     coverage: {
+      provider: "v8",
       include: ["src/**/*.js", "src/**/*.ts"],
       exclude: ["src/**/*.test.js", "src/**/*.test.ts"],
     },
     testTimeout: 15000, // 15 seconds for CLI operations
     passWithNoTests: true,
-    tsconfig: './tsconfig.test.json',
   },
 });
